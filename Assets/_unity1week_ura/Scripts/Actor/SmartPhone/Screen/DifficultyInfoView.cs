7fa9d3d17dbadda7df38d18a9dbeba164a4f9e25
hijoushiki <kagemakopf@gmail.com>
2026-03-24 02:55:59 +0900

    難易度情報のテキストの色を修正

--- Assets/_unity1week_ura/Scripts/Actor/SmartPhone/Screen/DifficultyInfoView.cs
+++ Assets/_unity1week_ura/Scripts/Actor/SmartPhone/Screen/DifficultyInfoView.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Text;
 using TMPro;
 using Unity1Week_Ura.Core;
 using UnityEngine;
@@ -8,6 +9,8 @@ namespace Unity1Week_Ura.Actor
     public class DifficultyInfoView : MonoBehaviour
     {
         [SerializeField] TMP_Text detailText;
+        [SerializeField] Color labelColor = Color.white;
+        [SerializeField] Color valueColor = Color.white;
 
         public void SetGameRule(GameRuleSO gameRule)
         {
@@ -16,30 +19,53 @@ namespace Unity1Week_Ura.Actor
                 return;
             }
 
-            detailText.text = ConvertDigitsToFullWidth(BuildDetailText(gameRule));
+            detailText.richText = true;
+            detailText.text = BuildDetailText(gameRule, labelColor, valueColor);
         }
 
-        static string BuildDetailText(GameRuleSO gameRule)
+        static string BuildDetailText(GameRuleSO gameRule, Color labelColor, Color valueColor)
         {
             if (gameRule == null)
             {
                 return string.Empty;
             }
 
-            var timeText = FormatTimeMinutes(gameRule.TimeLimitSeconds);
-            var speedText = string.IsNullOrWhiteSpace(gameRule.PostPerSecondDescription)
+            var timeText = ConvertDigitsToFullWidth(FormatTimeMinutes(gameRule.TimeLimitSeconds));
+            var speedText = ConvertDigitsToFullWidth(string.IsNullOrWhiteSpace(gameRule.PostPerSecondDescription)
                 ? "-"
-                : gameRule.PostPerSecondDescription;
-            var adsText = string.IsNullOrWhiteSpace(gameRule.AdvertisePostProbabilityDescription)
+                : gameRule.PostPerSecondDescription);
+            var adsText = ConvertDigitsToFullWidth(string.IsNullOrWhiteSpace(gameRule.AdvertisePostProbabilityDescription)
                 ? "-"
-                : gameRule.AdvertisePostProbabilityDescription;
-            var accountCount = gameRule.UsedAccounts?.Count ?? 0;
+                : gameRule.AdvertisePostProbabilityDescription);
+            var accountCountText = ConvertDigitsToFullWidth((gameRule.UsedAccounts?.Count ?? 0).ToString(CultureInfo.InvariantCulture));
+            var labelColorCode = ColorUtility.ToHtmlStringRGBA(labelColor);
+            var valueColorCode = ColorUtility.ToHtmlStringRGBA(valueColor);
 
-            return
-                $"時間: 　{timeText}分\n" +
-                $"流速: 　{speedText}\n" +
-                $"広告: 　{adsText}\n" +
-                $"操作垢: {accountCount}つ";
+            var builder = new StringBuilder();
+            AppendLine(builder, "時間", $"　{timeText}分", labelColorCode, valueColorCode);
+            AppendLine(builder, "流速", $"　{speedText}", labelColorCode, valueColorCode);
+            AppendLine(builder, "広告", $"　{adsText}", labelColorCode, valueColorCode);
+            AppendLine(builder, "操作垢", $"{accountCountText}つ", labelColorCode, valueColorCode);
+            return builder.ToString();
+        }
+
+        static void AppendLine(StringBuilder builder, string label, string value, string labelColorCode, string valueColorCode)
+        {
+            if (builder.Length > 0)
+            {
+                builder.Append('\n');
+            }
+
+            builder.Append("<color=#")
+                .Append(labelColorCode)
+                .Append('>')
+                .Append(label)
+                .Append(":</color>")
+                .Append("<color=#")
+                .Append(valueColorCode)
+                .Append('>')
+                .Append(value)
+                .Append("</color>");
         }
 
         static string FormatTimeMinutes(float timeLimitSeconds)
